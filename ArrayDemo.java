fce3ce72cf349f266f3d071fa955c1f2c9583c4f
rajkumarpamu <30862376+rajkumarpamu@users.noreply.github.com>
2021-05-15 17:34:26 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,27 @@
+
+
+   class ArrayDemo{
+
+
+  public static void main(String ar[]){
+
+
+     char name[] = new char[3];
+
+     name[0]='r';
+     name[1]='a';
+     name[2]='m';
+
+     for(int i=2;i>=0;i--){
+
+     System.out.println(name[i]);
+
+     }
+
+
+
+  }
+
+
+
+  }
